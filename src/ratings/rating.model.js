9a243b5977a5886496bc02571ab31a0a5704ff37
breynerbd <bbenitez-2021497@kinal.edu.gt>
2026-07-31 16:49:35 -0600

    SSL condicional para desarrollo local, actualizaciones de reseñas a zonas

--- src/ratings/rating.model.js
+++ src/ratings/rating.model.js
@@ -1,29 +1,47 @@
 import { DataTypes } from "sequelize";
 import { db } from "../../configs/db.js";
 
-export const Rating = db.define("ratings", {
-
+export const Rating = db.define("ratings_v2", {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true
     },
-
     score: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        validate: { min: 1, max: 5 }
     },
-
+    comment: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
     user_id: {
         type: DataTypes.INTEGER,
         allowNull: false
     },
-
     report_id: {
         type: DataTypes.INTEGER,
-        allowNull: false
+        allowNull: true
+    },
+    zone_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true
     }
-
 }, {
-    timestamps: true
+    tableName: "ratings_v2",
+    timestamps: true,
+    validate: {
+        exactlyOneTarget() {
+            const reportId = this.report_id || (this.dataValues && this.dataValues.report_id);
+            const zoneId = this.zone_id || (this.dataValues && this.dataValues.zone_id);
+
+            if (!reportId && !zoneId) {
+                throw new Error("Debes indicar report_id o zone_id");
+            }
+            if (reportId && zoneId) {
+                throw new Error("Un rating no puede calificar un reporte y una zona a la vez");
+            }
+        }
+    }
 });
