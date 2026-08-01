810cd757408ae68afe7abc03cc11084cb73747c5
breynerbd <bbenitez-2021497@kinal.edu.gt>
2026-07-31 19:09:41 -0600

    Correción de ratings no listados

--- src/ratings/rating.controller.js
+++ src/ratings/rating.controller.js
@@ -4,10 +4,37 @@ import { Report } from "../reports/report.model.js";
 
 export const getRatings = async (req, res) => {
     try {
-        const ratings = await Rating.findAll();
-        res.json(ratings);
+        const ratings = await Rating.findAll({
+            attributes: [
+                'report_id',
+                [Sequelize.fn('AVG', Sequelize.col('score')), 'average'],
+                [Sequelize.fn('COUNT', Sequelize.col('ratings_v2.id')), 'totalRatings'],
+                [Sequelize.fn('SUM', Sequelize.literal('CASE WHEN score = 5 THEN 1 ELSE 0 END')), 'count5'],
+                [Sequelize.fn('SUM', Sequelize.literal('CASE WHEN score = 4 THEN 1 ELSE 0 END')), 'count4'],
+                [Sequelize.fn('SUM', Sequelize.literal('CASE WHEN score = 3 THEN 1 ELSE 0 END')), 'count3'],
+                [Sequelize.fn('SUM', Sequelize.literal('CASE WHEN score = 2 THEN 1 ELSE 0 END')), 'count2'],
+                [Sequelize.fn('SUM', Sequelize.literal('CASE WHEN score = 1 THEN 1 ELSE 0 END')), 'count1'],
+            ],
+            include: [{
+                model: Report,
+                as: 'report',
+                attributes: ['id', 'title', 'description', 'zone_id']
+            }],
+            group: [
+                'ratings_v2.report_id',
+                'report.id',
+                'report.title',
+                'report.description',
+                'report.zone_id'
+            ],
+            raw: false,
+            nest: true
+        });
+
+        return res.status(200).json(ratings);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        console.error(error);
+        return res.status(500).json({ message: error.message });
     }
 };
 
@@ -23,7 +50,7 @@ export const getAverageRatingByReport = async (req, res) => {
                 as: 'report',
                 attributes: ['id', 'title', 'description']
             }],
-            group: ['ratings.id', 'report.id'],
+            group: ['Rating.report_id', 'report.id', 'report.title', 'report.description'],
             raw: false,
             nest: true
         });
